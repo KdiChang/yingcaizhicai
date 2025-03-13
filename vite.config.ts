eae9ca1115e7ee328c379dc8cce9665e0e6e6bcf
Penny <changjian.pan@kongsbergdigital.com>
2025-03-13 13:41:21 +0800

    1341

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-
-module.exports = {
-  publicPath: '/yingcaizhicai/', // 替换为你的仓库名
-  outputDir: 'dist'
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/yingcaizhicai/', // 替换为你的仓库名（替代原来的publicPath）
   plugins: [
     vue(),
   ],
@@ -18,5 +12,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'dist' // 替代原来的outputDir
   }
 })
