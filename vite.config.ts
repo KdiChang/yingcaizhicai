8913824c704b1d142a409750d3184045d3d6a3e3
Penny <changjian.pan@kongsbergdigital.com>
2025-03-13 13:39:58 +0800

    1339

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+module.exports = {
+  publicPath: '/yingcaizhicai/', // 替换为你的仓库名
+  outputDir: 'dist'
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
